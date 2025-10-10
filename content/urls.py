9d0eefc986f01d69415f1bc058acb2e0ae90e64b
Vik-tor39 <victor.suquilanda@udla.edu.ec>
2025-10-10 03:36:00 -0500

    Se implementó el formulario para adicionar un nuevo registro y se definieron los templates correspondientes.

--- content/urls.py
+++ content/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('list/', ContentListView.as_view(), name='contenidos')
+    path('list/', ContentListView.as_view(), name='contenidos'),
+    path('add/', ContentCreateView.as_view(), name='create'),
+    path('detail/<int:pk>/<slug:content_slug>', ContentDetailView.as_view(), name='detail'),
 ]
--- content/views.py
+++ content/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Contenido
 
 # Create your views here.
+
+# View para lista de contenido
 class ContentListView(ListView):
+    model = Contenido
+    paginate_by = 2
+
+# View para crear nuevo contenido 
+class ContentCreateView(CreateView):
+    model = Contenido
+    fields = ["title", 'content', 'image']
+    success_url = reverse_lazy('contenidos')
+
+# View para visualizar contenido en detalle
+class ContentDetailView(DetailView):
     model = Contenido
